80d9c0d5d8782ac696d0faf80ed6635915c031f6
Kavya <2k22cse077@kiot.ac.in>
2026-03-10 12:56:52 +0530

    added frontend

---
+++ event/src/services/bookingService.js
@@ -0,0 +1,11 @@
+import React from 'react';
+  
+  const BookingService = () =>  {
+	return (
+	  <div>
+	  </div>
+	);
+  }
+  
+  export default BookingService;
+  
---
+++ event/src/services/eventService.js
@@ -0,0 +1,11 @@
+import React from 'react';
+  
+  const EventService = () =>  {
+	return (
+	  <div>
+	  </div>
+	);
+  }
+  
+  export default EventService;
+  
